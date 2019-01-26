10dea8e01b1b0aedf6e3bce91e681da2dac1ce72
creatorpark <creatorpark@DESKTOP-MQFF84T>
2019-01-27 00:22:01 +0900

    first commit.

---
+++ src/main/java/sample/JpaSampleApplication.java
@@ -0,0 +1,14 @@
+package sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSampleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/sample/JpaSampleApplicationTests.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
