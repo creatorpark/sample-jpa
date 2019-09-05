818998fbd3d68b4aaf7e935d5c620771b5432cd2
creatorpark <creatorpark@naver.com>
2019-09-05 19:36:45 +0900

    team bi 살리기

--- src/main/java/creatorpark/jpa/app/entity/associations/o_n/bi/TeamBiRepository.java
+++ src/main/java/creatorpark/jpa/app/entity/associations/o_n/bi/TeamBiRepository.java
@@ -2,6 +2,8 @@ package creatorpark.jpa.app.entity.associations.o_n.bi;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import creatorpark.jpa.infra.app.GenericRepository;
@@ -11,5 +13,10 @@ public interface TeamBiRepository extends GenericRepository<TeamBi, Long> {
 
 	List<TeamBi> findByPlayers_uniformNumber(String uniformNumber);
 
+	@Query("SELECT t, p "
+	    + "   FROM TeamBi t JOIN FETCH PlayerBi p on t.id = p.team "
+		+ "  WHERE t.id=:id "
+		+ "    AND p.uniformNumber = :uniformNumber ")
+	TeamBi findRyuTeam(@Param("id") Long id,@Param("uniformNumber")String uniformNumber);
 }
  
